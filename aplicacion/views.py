8265e2bf2ea975ca553bf6077b1e78147919d48d
Adriel Viola <adriel.viola92@gmail.com>
2023-06-12 13:31:50 -0300

    DecimoTercer Commit

--- aplicacion/views.py
+++ aplicacion/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render
 
 def inicio(request):
-    return render(request, "aplicacion/index.html")
+    return render(request, "aplicacion/inicio.html")
 
 def notebook(request):
-    return render(request, "aplicacion/index.html")
+    return render(request, "aplicacion/notebook.html")
 
 def aio(request):
     return render(request, "aplicacion/aio.html")
 
 def desktop(request):
-    return render(request, "aplicacion/index.html")
+    return render(request, "aplicacion/desktop.html")
 
 def celulares(request):
-    return render(request, "aplicacion/index.html")
+    return render(request, "aplicacion/celulares.html")
 
