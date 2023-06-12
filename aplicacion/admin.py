28145766fa5279044068a03ab996f4f9e6896b49
Adriel Viola <adriel.viola92@gmail.com>
2023-06-11 21:00:10 -0300

    Decimo Primero

--- aplicacion/admin.py
+++ aplicacion/admin.py
@@ -1,8 +1,33 @@
 from django.contrib import admin
 from .models import notebook, aio, desktop, celulares
 
-admin.site.register(notebook)
-admin.site.register(aio)
-admin.site.register(desktop)
-admin.site.register(celulares)
+
+@admin.register(aio)
+class aioAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'apellido','marca','modelo','serie']
+    search_fields = ('nombre', 'apellido','marca','modelo','serie',)
+    ordering =  ('nombre', 'apellido','marca','modelo','serie',)
+    List_editable = ['nombre', 'apellido','marca','modelo','serie']
+
+@admin.register(notebook)
+class notebookAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'apellido','marca','modelo','serie']
+    search_fields = ('nombre', 'apellido','marca','modelo','serie',)
+    ordering = ('nombre', 'apellido','marca','modelo','serie',)
+    List_editable = ['nombre', 'apellido','marca','modelo','serie']
+
+@admin.register(desktop)
+class desktopAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'apellido','marca','modelo','serie']
+    search_fields = ('nombre', 'apellido','marca','modelo','serie',)
+    ordering = ('nombre', 'apellido','marca','modelo','serie',)
+    List_editable = ['nombre', 'apellido','marca','modelo','serie']
+
+@admin.register(celulares)
+class celularesAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'apellido','marca','modelo','imei']
+    search_fields = ('nombre', 'apellido','marca','modelo','imei',)
+    ordering = ('nombre', 'apellido','marca','modelo','imei',)
+    List_editable = ['nombre', 'apellido','marca','modelo','imei']
+
 # Register your models here.
--- aplicacion/models.py
+++ aplicacion/models.py
@@ -1,7 +1,16 @@
 from django.db import models
+#from django.contrib.auth.models import User
+
 
 # Create your models here.
+class usuario(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    email = models.CharField(max_length=40)
+
+
 class notebook(models.Model):
+   # usuario_id = models.ForeignKey(usuario)
     marca = models.CharField(max_length=20)
     modelo = models.CharField(max_length=20)
     serie = models.CharField(max_length=50)
@@ -10,6 +19,7 @@ class notebook(models.Model):
     emai=models.EmailField()
 
 class desktop(models.Model):
+    #usuario_id = models.ForeignKey(usuario)
     marca = models.CharField(max_length=20)
     modelo = models.CharField(max_length=20)
     serie = models.CharField(max_length=50)
@@ -18,6 +28,7 @@ class desktop(models.Model):
     emai=models.EmailField()
 
 class aio(models.Model):
+    #usuario_id = models.ForeignKey(usuario)
     marca = models.CharField(max_length=20)
     modelo = models.CharField(max_length=20)
     serie = models.CharField(max_length=50)
@@ -31,5 +42,5 @@ class celulares(models.Model):
     serie = models.CharField(max_length=50)
     nombre= models.CharField(max_length=30)
     apellido= models.CharField(max_length=30)
-    imai=models.DecimalField(max_digits=20, decimal_places=0)
+    imei=models.DecimalField(max_digits=20, decimal_places=0)
     
